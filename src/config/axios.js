83813747bd60cf172c719f59729e9918290e6284
Mohammad Ubaid <ubaidmohd.786@gmail.com>
2021-03-30 06:13:29 +0530

    refresh token

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+import { baseUrl } from "constants/defaultValues";
 const AXIOS = axios.create();
 
+// request interceptor
 AXIOS.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -13,6 +14,41 @@ AXIOS.interceptors.request.use(
   },
   (error) => {
     Promise.reject(error);
+  }
+);
+
+// response interceptor
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      originalRequest.url === `${baseUrl}/auth/token/refresh/`
+    ) {
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem("refresh_token");
+      return axios
+        .post(`${baseUrl}/auth/token/refresh/`, {
+          refresh: refreshToken,
+        })
+        .then((res) => {
+          if (res.status === 201) {
+            localStorage.setItem("token", res.data.access);
+            axios.defaults.headers.common["Authorization"] =
+              "Bearer " + localStorage.getItem("token");
+            return axios(originalRequest);
+          }
+        });
+    }
+    return Promise.reject(error);
   }
 );
 
